3fca873dd40f4a64efc65165c44a58cf0ee1389f
Pawan Katre <katrepawan73@gmail.com>
2025-12-16 16:48:18 +0530

    Added pytest screenshots and html report support

--- tests/login/test_login_invalid.py
+++
@@ -1,2 +0,0 @@
-def test_login_invalid():
-    assert True
--- tests/login/test_login_valid.py
+++ tests/login/test_login_valid.py
@@ -5,7 +5,23 @@ import time
 from utilities.webdriver_setup import get_driver
 from utilities.read_config import read_config
 from pages.login_page import LoginPage
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--start-maximized")
+
+    driver = webdriver.Chrome(
+        service=Service(ChromeDriverManager().install()),
+        options=options
+    )
+    yield driver
+    driver.quit()
 
 def test_valid_login():
     driver = get_driver()
